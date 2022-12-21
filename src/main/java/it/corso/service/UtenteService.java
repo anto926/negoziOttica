6e60e5db1503566e1066c01ba18c2676e33fb6f9
anto926 <anto_96_asr@live.it>
2022-12-21 11:08:16 +0100

    spring e db

--- src/main/java/it/corso/service/UtenteService.java
+++ src/main/java/it/corso/service/UtenteService.java
@@ -1,6 +1,8 @@
 package it.corso.service;
 
+import javax.servlet.http.HttpSession;
+
 public interface UtenteService {
 
-	
+	boolean verificaLogin(String username, String password, HttpSession session);
 }
